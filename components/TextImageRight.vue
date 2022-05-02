e7fe7e37c0c6af8a816156a0606b1442f8d8e093
blekso <blekepix@outlook.com>
2022-05-02 12:59:41 +0200

    added textimage

---
+++ components/TextImageRight.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="bg-gray-200 flex flex-col">
+    <!-- Container -->
+
+    <div class="container">
+      <div class="flex justify-center px-6 my-12">
+        <!-- Row -->
+        <div class="w-full xl:w-3/4 lg:w-11/12 flex rounded-lg">
+          <!-- Col -->
+
+          <div class="w-full lg:w-7/12 bg-white p-5 rounded-l-lg">
+            <div class="grid grid-cols-1 gap-12 md:grid-cols-2 lg:gap-20">
+              <div>
+                <div>
+                  <p
+                    class="mt-6 sm:mt-8 font-pangram-regular ptext-small text-text prose"
+                  >
+                    details
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="w-full h-auto bg-gray-400 hidden lg:block lg:w-5/12 bg-cover rounded-r-lg"
+            style="
+              background-image: url('https://source.unsplash.com/Mv9hjnEUHR4/600x800');
+            "
+          ></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TextImage",
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <Hero />
+    <TextImageLeft />
+    <TextImageRight />
+    <Footer />
   </div>
 </template>
 
